6c246f471c975e0121809e32e542cfd1a027218d
Herk, Rik van <rahvanherk@hotmail.com>
2023-12-15 17:49:32 +0100

    meh

--- day12_2.py
+++ day12_2.py
@@ -11,6 +11,12 @@ def check(p, i, c): #puzzle input coordinate
     return True
 
 def solve(puz, instr, ni, start):
+    if not instr:
+        if "#" in puz:
+            return np.array([0, 0])
+        else:
+            return np.array([1, 0])
+        
     s = np.zeros(2)
     si = start
     i = instr[ni]
@@ -21,7 +27,9 @@ def solve(puz, instr, ni, start):
             if "#" in puz[si:start]:
                 return s
             if ni == len(instr) - 1:
-                if start+lseg == len(puz):
+                if "#" in puz[start+lseg:]:
+                    pass
+                elif start+lseg == len(puz):
                     s[1] += 1
                 else:
                     s[0] += 1
@@ -43,50 +51,67 @@ instructions = []
 for line in lines:
     x = line.split(" ")
     puzzle.append(["."] + [i for i in x[0]] + ["."])
-    instructions.append([int(i) for i in x[1].split(",")])
-
+    instructions.append([int(i) for i in x[1].split(",")]*2)
 
 s = 0
-normal = []
-extra = []
+c0 = []
+c1 = []
+c2 = []
+c3 = []
+c4 = []
 t1 = perf_counter()
-for i in [1]:#range(1):
-    for j in range(10):
-        s = solve(puzzle[i], instructions[i]+instructions[i][0:j], 0, 0)
+for i in [11]:#range(1):
+    for j in range(len(instructions[i])):
+        s = solve(puzzle[i], instructions[i][:j], 0, 0)
         if sum(s) == 0:
-            break
+            if j>len(instructions)/5:
+                break
         else:
-            normal.append(np.insert(s,0,j))
-for i in [1]:#range(1):
-    for n in normal:
-        if n[1] > 0:
-            for j in range(10):
-                if j == 0:
-                    s = solve([".", "?"] + puzzle[i][1:], instructions[i], 0, 0)
-                else:
-                    s = solve([".", "?"] + puzzle[i][1:], instructions[i][-j:]+instructions[i], 0, 0)
-                if sum(s) == 0:
+            c0.append(np.insert(s,0,j))
+    for c in c0:
+        for j in [len(instructions[i])-int(c[0])-1]:#range(len(instructions[i])-int(c[0])):
+            s1 = solve(puzzle[i], instructions[i][int(c[0]):int(c[0])+j+1], 0, 0)
+            s2 = solve([".", "?"] + puzzle[i][1:], instructions[i][int(c[0]):int(c[0])+j+1], 0, 0)
+            if sum(s1) == 0 and sum(s2) == 0:
+                if j>len(instructions)/5:
                     break
-                else:
-                    extra.append(np.insert(s,0,j))
-    for k in range(j-1):
-        s = solve(puzzle[i], instructions[i][0:-(k+1)], 0, 0)
-        if sum(s) == 0:
-            break
-        else:
-            normal.append(np.insert(s,0,-k-1))
+            else:
+                s = np.zeros(2)
+                s[0] = s2[0]*c[1] + s1[0]*c[2]
+                s[1] = s2[1]*c[1] + s1[1]*c[2]
+                c1.append(np.insert(s,0,j+int(c[0])+1))
+        
+                
+# for i in [1]:#range(1):
+#     for n in c01:
+#         if n[1] > 0:
+#             for j in range(10):
+#                 if j == 0:
+#                     s = solve([".", "?"] + puzzle[i][1:], instructions[i], 0, 0)
+#                 else:
+#                     s = solve([".", "?"] + puzzle[i][1:], instructions[i][-j:]+instructions[i], 0, 0)
+#                 if sum(s) == 0:
+#                     break
+#                 else:
+#                     c12.append(np.insert(s,0,j))
+#     for k in range(j-1):
+#         s = solve(puzzle[i], instructions[i][0:-(k+1)], 0, 0)
+#         if sum(s) == 0:
+#             break
+#         else:
+#             c01.append(np.insert(s,0,-k-1))
 
-s = 0 
-for a in range(len(normal)):
-    z = normal[a][0]
-    for c in range(len(normal)):
-        if normal[c][0] + z == 0:        
-            s += normal[a][2]*np.sum(normal[c][1:])
-            break
-    for b in range(len(extra)):
-        if z + extra[b][0] == 0:
-            s += normal[a][1]*np.sum(extra[b][1:])
-            break
+# s = 0 
+# for a in range(len(c01)):
+#     z = c01[a][0]
+#     for c in range(len(c01)):
+#         if c01[c][0] + z == 0:        
+#             s += c01[a][2]*np.sum(c01[c][1:])
+#             break
+#     for b in range(len(c12)):
+#         if z + c12[b][0] == 0:
+#             s += c01[a][1]*np.sum(c12[b][1:])
+#             break
 
 
-print(s)
+# print(s)
